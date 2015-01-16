82454190d0477820b0a65d4ae88b79fdedfddb00
黃弘源 <eric@kingwaytek.com>
2015-01-16 16:21:47 +0800

    第一次上傳到gitHub

---
+++ gitHub_TestMVCApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gitHub_TestMVCApplication.Startup))]
+namespace gitHub_TestMVCApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
